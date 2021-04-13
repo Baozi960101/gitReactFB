9114ac0739fc0a95380271899a94afda27081532
brucetseng <bruce.tseng@bytedance.com>
2021-04-14 00:07:53 +0800

    login 登入ＲＲＲＲＲ
    Change-Id: I0330d1838640a3ca0df6beeab98b57955b79dfad

--- backend/indexBruce.js
+++ backend/indexBruce.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
+// https://www.npmjs.com/package/express
 const express = require('express')
-const mysql = require('mysql');
+const mysqlTool = require('mysql');
 
 function allowOrigin(res) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -10,7 +11,7 @@ function allowOrigin(res) {
 const app = express()
 
 // ---------開始連接資料庫---------
-const connection = mysql.createConnection(process.env.MYSQL_URL);
+const connection = mysqlTool.createConnection(process.env.MYSQL_URL);
 // ---------連接成功後做的事情---------
 // callback
 connection.connect(function(err) {
@@ -44,14 +45,28 @@ connection.connect(function(err) {
 // 判斷是否登入了的api
 app.get('/login/status', function(request, response) {
   allowOrigin(response)
-  response.json({
-    status: true // false 未登入
-  })
-  // response.send("<p style='color: red'>我五我我ㄛ</p>")
+  // querystring
+  const { a1, a2 } = request.query
+  // 補資料庫的邏輯
+  connection.query(`SELECT * FROM user where account='${a1}' and password='${a2}'`, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    if (results.length > 0) {
+      response.json({
+        status: true // false 未登入
+      })
+    } else {
+      response.json({
+        status: false // false 未登入
+      })
+    }
+  });
 })
 
 
-// ---------啟動伺服器---------
+// ---------啟動伺服器--------- port 3001
 app.listen(3001, () => {
   console.log(`server 啟動了！`)
 })
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => {
-  return <div>Login</div>;
+export default ({ setAccountPassword }) => {
+
+  const [a1, setA1] = useState('');
+  const [a2, setA2] = useState('');
+
+  function submit() {
+    setAccountPassword({
+      account: a1,
+      password: a2
+    })
+    setA1('')
+    setA2('')
+  }
+
+  function a1Change(e) {
+    setA1(e.target.value)
+  }
+
+  function a2Change(e) {
+    setA2(e.target.value)
+  }
+  return <div>
+    <input type="text" value={a1} onChange={a1Change}/>
+    <input type="password" value={a2} onChange={a2Change}  />
+    <button onClick={submit}>登入</button>
+  </div>;
 };
