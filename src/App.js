7a5860346df93945ea4a065a00eb281ffc470bc3
brucetseng <bruce.tseng@bytedance.com>
2021-04-17 00:38:53 +0800

    0417
    Change-Id: I1f6ef0a268c8f4941615930245ed0e1935d0370b

--- src/App.js
+++ src/App.js
@@ -4,70 +4,90 @@ import {
   Switch,
   Route,
   Redirect,
-  useHistory
+  useHistory,
 } from "react-router-dom";
 
 import Home from "./pages/home";
 import Login from "./pages/login";
 
 function App() {
-
   const history = useHistory();
 
-  const [isLogin, setIsLogin] = useState(false);
-  const [accountPassword, setAccountPassword] = useState({ account: '', password: '' })
+  const [isLogin, setIsLogin] = useState(0);
+  const [accountPassword, setAccountPassword] = useState({
+    account: "",
+    password: "",
+  });
 
   useEffect(() => {
-    if (accountPassword.account === '' || accountPassword.password === '') {
-      return
+
+    const token = window.localStorage.getItem("loginToken");
+    if ((accountPassword.account === "" || accountPassword.password === "") && !token) {
+      return;
     }
-    
-    fetch(`http://localhost:3001/login/status?a1=${accountPassword.account}&a2=${accountPassword.password}`).then(function (response) {
-      return response.json()
-    }).then(data => {
-      const { status = false } = data // 預設flase，避免後端掛掉
-      setIsLogin(status)
-      if (!status) {
-        window.alert("帳號密碼錯誤")
-      }
+
+    fetch(`http://localhost:3001/login`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        credentials: "include",
+      },
+      body: JSON.stringify({
+        a1: accountPassword.account,
+        a2: accountPassword.password,
+        token: token,
+      }),
     })
-  }, [accountPassword.account, accountPassword.password])
+      .then(function (response) {
+        return response.json();
+      })
+      .then((data) => {
+        const { message, status, userToken } = data; // 預設flase，避免後端掛掉
+        if (userToken) {
+          window.localStorage.setItem("loginToken", userToken);
+        }
+        setIsLogin(status);
+        if (!status) {
+          window.alert(message);
+        }
+      });
+  }, [accountPassword.account, accountPassword.password]);
 
   // isLogin變動後觸發，解決下面Route的render只會射後不理的問題（只執行一次）
   useEffect(() => {
     if (isLogin) {
-      history.push('/home')
+      history.push("/home");
     } else {
-      history.push('/')
+      history.push("/");
     }
-  }, [isLogin])
+  }, [isLogin]);
 
   return (
-      <Switch>
-        {/* 一開始的保護，沒登入去login */}
-        <Route
-          path="/home"
-          render={function ({ location }) {
-            return isLogin ? (
-              <Home />
-            ) : (
-              <Redirect
-                to={{
-                  pathname: "/",
-                  state: { from: location },
-                }}
-              />
-            );
-          }}
-        />
-        {/* <Route path="/home">
+    <Switch>
+      {/* 一開始的保護，沒登入去login */}
+      <Route
+        path="/home"
+        render={function ({ location }) {
+          return isLogin ? (
+            <Home />
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { from: location },
+              }}
+            />
+          );
+        }}
+      />
+      {/* <Route path="/home">
           <Home />
         </Route> */}
 
-        <Route path="/">
-          <Login setAccountPassword={setAccountPassword} />
-        </Route>
-      </Switch>
+      <Route path="/">
+        <Login setAccountPassword={setAccountPassword} />
+      </Route>
+    </Switch>
   );
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,29 +1,55 @@
 import React, { useState } from "react";
 
 export default ({ setAccountPassword }) => {
-
-  const [a1, setA1] = useState('');
-  const [a2, setA2] = useState('');
+  const [a1, setA1] = useState("");
+  const [a2, setA2] = useState("");
 
   function submit() {
     setAccountPassword({
       account: a1,
-      password: a2
+      password: a2,
+    });
+    setA1("");
+    setA2("");
+  }
+
+  function register() {
+    if (a1 === "" || a2 === "") {
+      alert("輸入帳密");
+      return;
+    }
+    fetch("http://localhost:3001/register", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        username: a1,
+        pwd: a2,
+      }),
     })
-    setA1('')
-    setA2('')
+      .then(function (response) {
+        return response.json();
+      })
+      .then((data) => {
+        // TODO del
+        console.log("data", data);
+      });
   }
 
   function a1Change(e) {
-    setA1(e.target.value)
+    setA1(e.target.value);
   }
 
   function a2Change(e) {
-    setA2(e.target.value)
+    setA2(e.target.value);
   }
-  return <div>
-    <input type="text" value={a1} onChange={a1Change}/>
-    <input type="password" value={a2} onChange={a2Change}  />
-    <button onClick={submit}>登入</button>
-  </div>;
+  return (
+    <div>
+      <input type="text" value={a1} onChange={a1Change} />
+      <input type="text" value={a2} onChange={a2Change} />
+      <button onClick={submit}>登入</button>
+      <button onClick={register}>註冊</button>
+    </div>
+  );
 };
