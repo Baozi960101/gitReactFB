8fd597011d534ed47a9a530ae4d7f0ac27077ea9
Baozi960101 <Baozi960101@gmail.com>
2021-04-24 19:32:17 +0800

    0424

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,10 @@ import Main from "../components/Main";
 export default () => {
   return (
     <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      />
       <Header />
       <Main />
     </>
